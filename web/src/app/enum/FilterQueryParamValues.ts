c616ca5f81a187a02d9888fe2daec35068a985e5
Alberto Morón Hernández <alberto.moronh@gmail.com>
2021-02-07 20:30:04 +0000

    Improve palette switching code

--- web/src/app/enum/FilterQueryParamValues.ts
+++ web/src/app/enum/FilterQueryParamValues.ts
@@ -1,4 +1,9 @@
 
+export enum FilterTrayOpen {
+  o,  // open
+  c,  // closed
+}
+
 // ----- 1/3 FILTER ------------------------------------------------------------
 
 export enum FilterSentiment {  // fms:
@@ -17,18 +22,20 @@ export enum FilterMarketCap {  // fcp:
 // ----- 2/3 SORT --------------------------------------------------------------
 
 export enum SortBy {  // so:
-  al,  // alphabetical
   ca,  // market cap
   sh,  // short ratio
   fl,  // short % of float
+  rp,  // revenue per share
+  al,  // alphabetical
 }
 
 
 // ----- 3/3 VISUALISE ---------------------------------------------------------
 
 export enum Visualise {  // vi:
-  ms,  // market sentiment
   ca,  // market cap
   sh,  // short ratio
+  fl,  // short % of float
   rp,  // revenue per share
+  ms,  // market sentiment
 }
