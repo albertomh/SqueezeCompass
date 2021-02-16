3836abaca75c8a3476d63ff4867165142a777f89
Alberto Morón Hernández <alberto.moronh@gmail.com>
2021-02-16 17:49:30 +0000

    1.0.0

--- web/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-
-  filePathForConstituents: './assets/data/constituents/2021-02-15_constituents.json',
-  filePathForSnapshot: './assets/data/snapshots/2021-02-15_snapshot.json',
-};
